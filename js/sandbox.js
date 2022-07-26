9ead9a0883be776b7cfbb9b42f717f639b548846
eliyahukoren <50693235+eliyahukoren@users.noreply.github.com>
2022-07-26 20:01:44 +0300

    Initial commit
    Just add files into repository

---
+++ js/sandbox.js
@@ -0,0 +1,8 @@
+// const siteTitle = 'Site Layout N.1'
+
+// setInterval(() => {
+//   setTimeout(() => {
+//     document.title = siteTitle
+//   }, 1500)
+//   document.title = 'New Title'
+// }, 1000)
